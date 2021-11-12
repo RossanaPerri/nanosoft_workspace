041c1f988c1c6db2a41af39d8b0c0c90cfe40351
emanuel <emicovi@gmail.com>
2021-11-12 17:48:17 +0100

    Prova connessione al db con driverManager

--- src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
+++ src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
@@ -1,7 +1,34 @@
 package it.nanosoft.mechAdvisor.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class PostgreSqlConnection {
 	
+	private String url = "jdbc:postgresql://35.180.170.147:5432/postgres";
+	private String user = "postgres";
+	private String password = "!2020!pg";
 	
+	
+	private void connect() {
+		try(Connection connection = DriverManager.getConnection(url, user, password)){
+			if(connection!=null) {
+				System.out.println("Connected to PostgreSql!");
+			}else {
+				System.out.println("Failed to connect to the DB");
+			}
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
+}
+	
+	public static void main(String[] args) {
+		PostgreSqlConnection sqlConnection = new PostgreSqlConnection();
+		
+		sqlConnection.connect();
+	}
 }
