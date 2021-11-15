1bd3ce93912bef69042d43d7f37cbd3f3f87e872
emanuel <emicovi@gmail.com>
2021-11-15 16:35:23 +0100

    Database connection class singleton

--- src/main/java/it/nanosoft/App.java
+++ src/main/java/it/nanosoft/App.java
@@ -1,26 +1,78 @@
 package it.nanosoft;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.print.attribute.standard.DateTimeAtCompleted;
 
 import it.nanosoft.mechAdvisor.connection.PostgreSqlConnection;
 
 public class App {
-	public static void main(String[] args) {
-			PostgreSqlConnection sqlConnection = new PostgreSqlConnection();
-			
+	public static void main(String[] args) throws ClassNotFoundException, SQLException, IOException {
+
+		App app = new App();
+
+		// System.out.println(app.getUtenteCount());
+
+		// 1) Tutti gli utenti che hanno cambiato auto negli ultimi 2 anni la cui auto
+		// primaria era un AUDI
+		app.query1();
+//		// 2) Le migliori 10 officine, calcolando il voto medio di ogni officina
+//		app.query2();
+//		// 3)Tutti gli utenti della provincia di CZ che hanno effettuato almeno una
+//		// sostituzione
+//		app.query3();
+//		// 4) Tutti i meccanici che possiedono le officine migliori
+//		app.query4();
+//		// 5)Gli utenti di tipo admin che sono anche utenti dell'applicazione che hanno
+//		// lasciato almeno una recensione positiva ( il cui valore è >=6 )
+//		app.query5();
+//		// 6) I 20 utenti che hanno lasciato più recensioni negative
+//		app.query6();
+//		// 8) Tutti gli utenti che in possesso di un motorino che hanno lasciato almeno
+//		// una recensione negativa nella provincia di CS
+//		app.query8();
+
+	}
+
+	/**
+	 * Questo metodo restituisce tutti gli utenti che hanno cambiato auto negli
+	 * ultimi 2 anni la cui auto primaria era un AUDI
+	 * 
+	 * @throws ClassNotFoundException
+	 * @throws SQLException
+	 * @throws IOException
+	 */
+
+	public void query1() {
+
+		String SQL = "\r\n" + "	select A.nome, A.cognome, U.email \r\n"
+				+ "	from utente as U, anagrafica as A, propieta_veicolo as PV, veicolo as V	\r\n"
+				+ "	where A.email_utente = U.email and PV.email_utente = U.email and PV.id_veicolo = V.id \r\n"
+				+ "		and PV.tipologia_veicolo = V.tipologia and PV.data_fine_prop <= '2019-11-11' and V.marca ='audi';";
+
+		Connection databaseConnection = null;
+		try {
+			databaseConnection = PostgreSqlConnection.getInstance().getConnection();
+			Statement stmt = databaseConnection.createStatement();
+			ResultSet rs = stmt.executeQuery(SQL);
+			while (rs.next()) {
+				System.out.println(rs.getString("nome") + " " + rs.getString("cognome"));
+			}
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
 			try {
-				sqlConnection.createConnection();
-			} catch (ClassNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				databaseConnection.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 
 	}
+
+}
--- src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
+++ src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
@@ -1,79 +1,61 @@
 package it.nanosoft.mechAdvisor.connection;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
-public class PostgreSqlConnection {
 
-	public String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm").format(new Date());
-    private final String createTable = "CREATE TABLE \"" + date + "\" (id INT, data VARCHAR(100));";
-	private static final int LoginTimeout = 10;
+	public class PostgreSqlConnection {
 
-	public Connection createConnection() throws IOException, ClassNotFoundException, SQLException {
-		Properties prop = new Properties();
-		System.out.println("\n\n=======================\nJDBC Connector Test " + date);
-		System.out.println("User home directory: " + System.getProperty("user.home"));
-		String host;
-		String username;
-		String password;
-		String driver;
-		try {
-		    File targetFile = new File("/Users/emicovi/Documents/workspace/nanosoft_MechAdvisor/src/main/resources/mydb.cfg");
-			prop.load(new java.io.FileInputStream(targetFile));
+	    private static PostgreSqlConnection instance;
+	    private Connection connection;
+	    String host = null;
+		String username = null;
+		String password = null;
+		String driver = null;
+		
+   
+	    
+	    private PostgreSqlConnection() throws SQLException {
+	        try {
+	    		Properties prop = new Properties();
+			    File targetFile = new File("/Users/emicovi/Documents/workspace/nanosoft_MechAdvisor/src/main/resources/mydb.cfg");
+			    try {
+					prop.load(new java.io.FileInputStream(targetFile));
+				} catch (FileNotFoundException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
 
-			host = prop.getProperty("host").toString();
-			username = prop.getProperty("username").toString();
-			password = prop.getProperty("password").toString();
-			driver = prop.getProperty("driver").toString();
-		} catch (IOException e) {
-			System.out.println("Unable to find mydb.cfg"
-					+ "\n Please make sure that configuration file created in this folder.");
+				host = prop.getProperty("host").toString();
+				username = prop.getProperty("username").toString();
+				password = prop.getProperty("password").toString();
+				driver = prop.getProperty("driver").toString();
 
-			host = "Unknown HOST";
-			username = "Unknown USER";
-			password = "Unknown PASSWORD";
-			driver = "Unknown DRIVER";
-		}
+	            Class.forName("org.postgresql.Driver");
+	            this.connection = DriverManager.getConnection(host, username, password);
+	        } catch (ClassNotFoundException ex) {
+	            System.out.println("Database Connection Creation Failed : " + ex.getMessage());
+	        }
+	    }
 
-		System.out.println(
-				"host: " + host + "\nusername: " + username + "\npassword: " + password + "\ndriver: " + driver);
+	    public Connection getConnection() {
+	        return connection;
+	    }
 
-		Class.forName(driver);
-		System.out.println("--------------------------");
-		System.out.println("DRIVER: " + driver);
-		System.out.println("Set Login Timeout: " + LoginTimeout);
-		DriverManager.setLoginTimeout(LoginTimeout);
-		Connection connection = DriverManager.getConnection(host, username, password);
-		System.out.println("CONNECTION: " + connection);
+	    public static PostgreSqlConnection getInstance() throws SQLException {
+	        if (instance == null) {
+	            instance = new PostgreSqlConnection();
+	        } else if (instance.getConnection().isClosed()) {
+	            instance = new PostgreSqlConnection();
+	        }
 
-		return connection;
-	}
+	        return instance;
+	    }
 	
-//	public String runSqlStatement() {
-//        String result = "";
-//        try {
-//            Statement statement = createConnection().createStatement();
-//            System.out.println("SQL query: " + createTable);
-//            statement.execute(createTable);
-//        } catch (IOException | ClassNotFoundException ex) {
-//            Logger.getLogger(DbManager.class.getName()).log(Level.SEVERE, null, ex);
-//            System.out.println("Exception occurred: " + ex);
-//            result = ex.getMessage();
-//        } catch (SQLException ex) {
-//            ex.printStackTrace();
-//            result = ex.getMessage();
-//        }
-//        return result;
-//    }
-//}
-//
 }
