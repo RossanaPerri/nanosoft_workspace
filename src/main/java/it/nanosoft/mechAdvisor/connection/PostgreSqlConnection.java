a7f9dadd9c6def1092724d30d1b4e765a8013563
emanuel <emicovi@gmail.com>
2021-11-15 12:35:21 +0100

    Aggiunta funzione per collegamento al file con i dati sensibili del db

--- src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
+++ src/main/java/it/nanosoft/mechAdvisor/connection/PostgreSqlConnection.java
@@ -1,8 +1,6 @@
 package it.nanosoft.mechAdvisor.connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.File;
 import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -17,7 +15,7 @@ import java.util.logging.Logger;
 public class PostgreSqlConnection {
 
 	public String date = new SimpleDateFormat("dd-MM-yyyy-HH-mm").format(new Date());
-	private final String createTable = "CREATE TABLE \"" + date + "\" (id INT, data VARCHAR(100));";
+    private final String createTable = "CREATE TABLE \"" + date + "\" (id INT, data VARCHAR(100));";
 	private static final int LoginTimeout = 10;
 
 	public Connection createConnection() throws IOException, ClassNotFoundException, SQLException {
@@ -29,14 +27,15 @@ public class PostgreSqlConnection {
 		String password;
 		String driver;
 		try {
-			prop.load(new java.io.FileInputStream(System.getProperty("user.home") + "/mydb.cfg"));
+		    File targetFile = new File("/Users/emicovi/Documents/workspace/nanosoft_MechAdvisor/src/main/resources/mydb.cfg");
+			prop.load(new java.io.FileInputStream(targetFile));
 
 			host = prop.getProperty("host").toString();
 			username = prop.getProperty("username").toString();
 			password = prop.getProperty("password").toString();
 			driver = prop.getProperty("driver").toString();
 		} catch (IOException e) {
-			System.out.println("Unable to find mydb.cfg in " + System.getProperty("user.home")
+			System.out.println("Unable to find mydb.cfg"
 					+ "\n Please make sure that configuration file created in this folder.");
 
 			host = "Unknown HOST";
@@ -58,4 +57,23 @@ public class PostgreSqlConnection {
 
 		return connection;
 	}
+	
+//	public String runSqlStatement() {
+//        String result = "";
+//        try {
+//            Statement statement = createConnection().createStatement();
+//            System.out.println("SQL query: " + createTable);
+//            statement.execute(createTable);
+//        } catch (IOException | ClassNotFoundException ex) {
+//            Logger.getLogger(DbManager.class.getName()).log(Level.SEVERE, null, ex);
+//            System.out.println("Exception occurred: " + ex);
+//            result = ex.getMessage();
+//        } catch (SQLException ex) {
+//            ex.printStackTrace();
+//            result = ex.getMessage();
+//        }
+//        return result;
+//    }
+//}
+//
 }
