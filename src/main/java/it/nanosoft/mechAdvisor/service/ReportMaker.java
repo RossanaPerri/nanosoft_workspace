1a835027a10a0da45709a710be5c07646c684d16
emanuel <emicovi@gmail.com>
2021-11-23 10:51:01 +0100

    salvataggio query utente su file excel

--- src/main/java/it/nanosoft/mechAdvisor/service/ReportMaker.java
+++ src/main/java/it/nanosoft/mechAdvisor/service/ReportMaker.java
@@ -1,27 +1,15 @@
 package it.nanosoft.mechAdvisor.service;
 
-import java.io.File;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.sql.ResultSet;
-import java.sql.ResultSetMetaData;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Objects;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
 
 import it.nanosoft.mechAdvisor.model.Utente;
 
@@ -33,121 +21,42 @@ import it.nanosoft.mechAdvisor.model.Utente;
  * @author emicovi
  */
 
-public class ReportMaker {
+public class ReportMaker implements Loggable {
 
-	private static final Workbook book = new XSSFWorkbook();
+	public void reportUtente(List<Utente> userList) throws IOException {
 
-	public void reportUtente(List<Utente> lu) throws IOException {
+		try {
+			InputStream fis = ReportMaker.class.getResourceAsStream("/reportUtenteTemplate.xlsx");
+			XSSFWorkbook wb = new XSSFWorkbook(fis);
+			fis.close();
+			XSSFSheet sh = wb.getSheetAt(q);
 
-		URL url = getClass().getClassLoader().getResource("reportUtenteTemplate.xlsx");
-
-		FileInputStream file = new FileInputStream(new File(url.getFile()));
-
-		// Create Workbook instance holding reference to .xlsx file
-		XSSFWorkbook workbook = new XSSFWorkbook(file);
-
-		// Get first/desired sheet from the workbook
-		XSSFSheet sheet = workbook.getSheetAt(0);
-
-		// Iterate through each rows one by one
-		Iterator<Row> rowIterator = sheet.iterator();
-
-		int rowIndex = 0;
-
-		Row row = sheet.createRow(++rowIndex);
-		while (lu.size() > 1) {
-			for (int i = 0; i < lu.size(); i++) {
-				row.createCell(i).setCellValue(lu.get(i).getNome());
-				rowIndex++;
+			int rownum = 1;
+			for (Utente user : userList) {
+				Row row = sh.createRow(rownum++);
+				createList(user, row);
 			}
-		}
-		File f = new File(
-				System.getProperty("user.home").concat(System.getProperty("file.separator")).concat("utente.xlsx"));
-
-		try (FileOutputStream fos = new FileOutputStream(f)){
-			book.write(fos);
+			FileOutputStream out = new FileOutputStream(System.getProperty("user.home").concat(System.getProperty("file.separator")).concat("reportUtente.xlsx")); // file name with path
+			wb.write(out);
+			out.close();
+			wb.close();
+		} catch (Exception e) {
+			newloggerApp.error(" ---- : ", e);
 		}
 	}
-}
 
-//	public void toExcel(ResultSet rs, String nq) throws FileNotFoundException, IOException, SQLException {		
-//		final ResultSetMetaData rsmd = rs.getMetaData();
-//		List<String> columns = new ArrayList<String>() {{
-//			for (int i=1; i <= rsmd.getColumnCount(); i++) {
-//				add(rsmd.getColumnLabel(i));
-//			}
-//		}};
-//
-//		try {
-//			Sheet sheet = book.createSheet(nq);
-//			Row header = sheet.createRow(0);
-//			for (int i=0; i<columns. size(); i++) {
-//				header.createCell(i).setCellValue(columns.get(i));
-//			}
-//			int rowIndex = 0;
-//			while(rs.next()) {
-//				Row row = sheet. createRow(++rowIndex);
-//				for (int i=0; i<columns. size(); i++) {
-//					row.createCell(i).setCellValue(Objects.toString(rs.getObject(columns.get(i)),""));
-//
-//				}
-//			}
-//		}catch (Exception e) {
-//			e.getMessage();
-//		}
-//
-//		File f = new File(System.getProperty("user.home").concat(System.getProperty("file.separator")).concat("query.xlsx"));
-//
-//
-//		try(FileOutputStream fos = new FileOutputStream (f))
-//
-//		{
-//			book.write (fos);
-//		}
-//
-//
-//		fromExcel();
-//	}
-//
-//	public void fromExcel() {
-//		try
-//		{
-//			FileInputStream file = new FileInputStream(new File("query.xlsx"));
-//
-//			//Create Workbook instance holding reference to .xlsx file
-//			XSSFWorkbook workbook = new XSSFWorkbook(file);
-//
-//			//Get first/desired sheet from the workbook
-//			XSSFSheet sheet = workbook.getSheetAt(0);
-//
-//			//Iterate through each rows one by one
-//			Iterator<Row> rowIterator = sheet.iterator();
-//			while (rowIterator.hasNext()) 
-//			{
-//				Row row = rowIterator.next();
-//				//For each row, iterate through all the columns
-//				Iterator<Cell> cellIterator = row.cellIterator();
-//
-//				while (cellIterator.hasNext()) 
-//				{
-//					Cell cell = cellIterator.next();
-//					//Check the cell type and format accordingly
-//					switch (cell.getCellType()) 
-//					{
-//					case NUMERIC:
-//						System.out.print(cell.getNumericCellValue() + "---");
-//						break;
-//					case STRING:
-//						System.out.print(cell.getStringCellValue() + "---");
-//						break;
-//					}
-//				}
-//				System.out.println("");
-//			}
-//			file.close();
-//		} 
-//		catch (Exception e) 
-//		{
-//			e.printStackTrace();
-//		}
-//	}
+// creating cells for each row
+	private static void createList(Utente user, Row row) {
+		Cell cell = row.createCell(0);
+		cell.setCellValue(user.getNome());
+		cell = row.createCell(1);
+		cell.setCellValue(user.getCognome());
+	}
+
+	@Override
+	public Logger logging() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
